4f9d04dc3faa0b153cceced2fc65a1d4349dfae3
RoseMagura <magurarm@guilford.edu>
2020-12-06 13:17:46 -0500

    Edit: Add Udacity solution and fix formatting

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,10 @@
 def binary_search(array, target):
     '''Write a function that implements the binary search algorithm using iteration
-   
+
     args:
       array: a sorted array of items of the same type
       target: the element you're searching for
-   
+
     returns:
       int: the index of the target, if found, in the source
       -1: if the target is not found
@@ -18,7 +18,7 @@ def binary_search(array, target):
             index += half
             return index
         elif target > checking:
-            array = array[half :]
+            array = array[half:]
             length = len(array)
             index += half
             if length == 1 and target != checking:
@@ -28,12 +28,35 @@ def binary_search(array, target):
             length = len(array)
     return -1
 
+
+def binary_search_udacity_solution(array, target):
+    start_index = 0
+    end_index = len(array) - 1
+
+    while start_index <= end_index:
+        mid_index = (start_index + end_index)//2
+
+        mid_element = array[mid_index]
+        # we have found the element
+        if target == mid_element:
+            return mid_index
+        # the target is less than mid element
+        elif target < mid_element:
+            # we will only search in the left half
+            end_index = mid_index - 1
+        # the target is greater than mid element
+        else:
+            # we will search only in the right half
+            start_index = mid_element + 1
+
+
 def test_function(test_case):
     answer = binary_search(test_case[0], test_case[1])
     if answer == test_case[2]:
         print("Pass!")
     else:
         print("Fail!")
+
 
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 4
@@ -51,4 +74,4 @@ array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 100
 index = -1
 test_case = [array, target, index]
-test_function(test_case) 
+test_function(test_case)
