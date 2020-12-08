e1f4e3b47735dc920ffca2f4763a8ebfff6bb80b
RoseMagura <magurarm@guilford.edu>
2020-12-07 21:59:46 -0500

    Edit: Add Udacity solution

--- case_sort.py
+++ case_sort.py
@@ -32,4 +32,29 @@ def case_sort(string):
             upper_index += 1
     return output
 
+def case_sort_udacity_solution(string):
+    upper_ch_index = 0
+    lower_ch_index = 0
+    
+    sorted_string = sorted(string)
+    for index, character in enumerate(sorted_string):
+        # check if character is lower-case
+        ascii_int = ord(character)
+        if 97 <= ascii_int <= 122:       # ASCII value of a = 97 & ASCII value of z = 122
+            lower_ch_index = index
+            break
+            
+    output = list()
+    for character in string:
+        ascii_int = ord(character)
+        # if character is lower case pick next lower_case character
+        if 97 <= ascii_int <= 122:
+            output.append(sorted_string[lower_ch_index])
+            lower_ch_index += 1
+        else:
+            output.append(sorted_string[upper_ch_index])
+            upper_ch_index += 1
+    return "".join(output)
+
 print(case_sort('fedRTSersUXJ') == 'deeJRSfrsTUX')
+print(case_sort('defRTSersUXI') == 'deeIRSfrsTUX')
