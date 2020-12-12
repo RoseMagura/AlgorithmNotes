3a6fba7e729b4eb12ebae0cd8e1d0c1379134ffa
RoseMagura <magurarm@guilford.edu>
2020-12-11 19:29:43 -0500

    Edit: Experimenting with trie

--- trie_introduction.py
+++ trie_introduction.py
@@ -6,20 +6,36 @@ class TrieNode(object):
 class Trie(object):
     def __init__(self):
         self.root = TrieNode()
+    #     self.root.children = {
+    #         'a': {
+    #     'd': {
+    #         'd': {'word_end': True},
+    #         'word_end': False},
+    #     'word_end': True},
+    # # hi word
+    # 'h': {
+    #     'i': {'word_end': True},
+    #     'word_end': False}}
     
     def add(self, word):
         """
         Add `word` to trie
         """
-        self.root.children[word[0]] = {
-            word[1]: {word[2]: {'word_end': True}, 'word_end': True},
-            'word_end': False
-        }
+        while len(word) > 0:
+            char = word[0]
+            self.root.children[char] = self.recursively_add(word[1:])
         print(self.root.children)
-        # word = word[1:]
-        # length = len(word)
-        # for i in range(length):
-        #     word[i]
+        # trie_words = self.root.children
+        # cur_layer = trie_words
+        # for char in word:
+        #     while char not in cur_layer:
+        #         print('not here')
+        #         # cur_layer = cur_layer[]
+        #         # cur_layer[char] = dict()
+        #     # cur_layer = trie_words[char]
+        # return trie_words
+    
+    def recursively_add(self, word):
         return
     
     def exists(self, word):
@@ -33,10 +49,20 @@ class Trie(object):
             node = node.children[char]
             print(node)
         return node['word_end']
+    
+    def traverse(self):
+        cur_layer = self.root
+        for item in cur_layer.children.keys():
+            print(cur_layer.children[item])
+        return
 
 
 # word_list = ['apple', 'bear', 'goo', 'good', 'goodbye', 'goods', 'goodwill', 'gooses'  ,'zebra']
 word_trie = Trie()
+
+basic_trie = Trie()
+
+basic_trie.traverse()
 
 # # Add words
 # for word in word_list:
@@ -51,4 +77,5 @@ word_trie = Trie()
 #         print('"{}" is not a word.'.format(word))
 
 word_trie.add('abc')
-print(word_trie.exists('abc'))
+# word_trie.add('bear')
+# print(word_trie.exists('abc'))
