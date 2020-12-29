146df3f0673390096d48526179993d28d974716d
RoseMagura <magurarm@guilford.edu>
2020-12-29 15:45:40 -0500

    Edit: Replace with Udacity solution

--- longest_palindromic_sequence.py
+++ longest_palindromic_sequence.py
@@ -1,30 +1,32 @@
-def lps(input_string): 
-    
+def lps(input_string):
+
     # TODO: Complete this implementation of the LPS function
     # The function should return one value: the LPS length for the given input string
     length = len(input_string)
     lookup_table = [[0 for x in range(length)] for x in range(length)]
-    for char_i, char in enumerate(input_string):
-        for char_j, char2 in enumerate(input_string):
-            if(char_i == char_j):
-                lookup_table[char_i][char_j] = 1
-            if(char == char2):
-                if char_i < char_j:
-                    print('MATCH with', str(char), str(char2))
-                    # print(char_j, char_i)
-                    bottom_left = lookup_table[char_i + 1][char_j - 1]
-                    print(lookup_table)
-                    # lookup_table[char_i][char_j] = bottom_left + 2
-                    lookup_table[char_i][char_j] = 100
-            else:
-                # print('NOT MATCH')
-                if char_i < char_j:
-                    left = lookup_table[char_i][char_j - 1]
-                    bottom = lookup_table[char_i + 1][char_j]
-                    lookup_table[char_i][char_j] = max(left, bottom)
 
-    print(lookup_table)
-    return 
+    for i in range(length):
+        lookup_table[i][i] = 1
+
+    for s_size in range(2, length+1):
+        for start_idx in range(length-s_size+1):
+            end_idx = start_idx + s_size - 1
+            if s_size == 2 and input_string[start_idx] == input_string[end_idx]:
+                # match with a substring of length 2
+                lookup_table[start_idx][end_idx] = 2
+            elif input_string[start_idx] == input_string[end_idx]:
+                # general match case
+                lookup_table[start_idx][end_idx] = lookup_table[start_idx+1][end_idx-1] + 2
+            else:
+                # no match case, taking the max of two values
+                lookup_table[start_idx][end_idx] = max(
+                    lookup_table[start_idx][end_idx-1], lookup_table[start_idx+1][end_idx])
+
+    # debug line
+    # pp.pprint(L)
+
+    return lookup_table[0][length-1]  # value in top right corner of matrix
+
 
 def test_function(test_case):
     string = test_case[0]
@@ -35,6 +37,7 @@ def test_function(test_case):
         print("Pass")
     else:
         print("Fail")
+
 
 lps('NAN')
 
