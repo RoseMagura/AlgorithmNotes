db56c3a707d1a9f189fb9436dc982edc8d65e99f
RoseMagura <magurarm@guilford.edu>
2020-12-30 16:25:52 -0500

    Feat: Add potential solution for coin exercise

--- coin_change.py
+++ coin_change.py
@@ -1,8 +1,22 @@
 def coin_change(coins, amount):
 
     # TODO: Complete the coin_change function
-    # This should return one value: the fewest coins needed to make up the given amount
-    pass
+    # This should return one value: the fewest coins needed to make up
+    # the given amount
+    number_coins = 0
+    if coins[0] > amount:
+        return -1
+    while amount:
+        current_value = coins.pop(-1)
+        remainder = amount % current_value
+        if current_value > amount:
+            continue
+        number_coins += amount // current_value
+        if remainder == 0:
+            break
+        amount = remainder
+    return number_coins
+
 
 def test_function(test_case):
     arr = test_case[0]
@@ -14,22 +28,21 @@ def test_function(test_case):
     else:
         print("Fail")
 
-coin_change([1, 2, 3], 6)
 
-arr = [1,2,5]
+arr = [1, 2, 5]
 amount = 11
 solution = 3
 test_case = [arr, amount, solution]
-# test_function(test_case)
+test_function(test_case)
 
-arr = [1,4,5,6]
+arr = [1, 4, 5, 6]
 amount = 23
 solution = 4
 test_case = [arr, amount, solution]
-# test_function(test_case)
+test_function(test_case)
 
-arr = [5,7,8]
+arr = [5, 7, 8]
 amount = 2
 solution = -1
 test_case = [arr, amount, solution]
-# test_function(test_case)
+test_function(test_case)
