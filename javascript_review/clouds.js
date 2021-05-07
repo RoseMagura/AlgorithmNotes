d5d842539e5ca5960d5ba2950f616bede89ae12a
RoseMagura <magurarm@guilford.edu>
2021-05-07 17:30:55 -0400

    Edit: Replace with more efficient solution

--- javascript_review/clouds.js
+++ javascript_review/clouds.js
@@ -1,45 +1,13 @@
 const jumpingOnClouds = c => {
-    let allPossible = [];
-    let currPos = 0;
-    let jumps = 0;
-    // one length jump
-    while(currPos < c.length){
-        if(c[currPos + 1] === 1) {
-            currPos += 2;
-            jumps++;
-            continue;
+    for (let i = 0; i < cloudLength - 1; i++) {
+        if ((i + 2) < cloudLength && c[(i + 2)] == 0) {    //Double
+            numOfJumps++;
+            i++;
+        } else {          //Single
+            numOfJumps++;
         }
-        currPos++;
-        if(currPos > c.length){
-            break;
-        }
-        jumps++;
     }
-    allPossible.push(jumps);
-    // reset
-    jumps = 0;
-    currPos = 0;
-    // two length jump
-    while(currPos < c.length){
-        console.log(c[currPos], currPos);
-        if(c[currPos + 2] === 1) {
-            console.log('danger');
-            currPos++;
-            jumps++;
-            continue;
-        }
-        currPos += 2;
-        if(currPos > c.length){
-            console.log('breaking', currPos);
-            break;
-        }
-        jumps++;
-
-    }
-    allPossible.push(jumps);
-    return allPossible[0] <= allPossible[1] 
-        ? allPossible[0]
-        : allPossible[1];
+    return numOfJumps;
 }
 
 // const result = jumpingOnClouds([0, 0, 0, 1, 0, 0]); // should return 3
