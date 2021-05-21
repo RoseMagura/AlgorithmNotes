1e42a270b9af012fd30efc7358ab161f1cff71f3
RoseMagura <magurarm@guilford.edu>
2021-05-21 15:44:03 -0400

    Feat: Add linked list review

--- javascript_review/linkedList.js
+++ javascript_review/linkedList.js
@@ -1,24 +1,40 @@
-// class Node {
-//     constructor(data) {
-//         this.data = data;
-//         this.next = null;
-//     }
-// }
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-// function Solution(){
+function Solution(){
 
-// 	this.insert=function(head,data){
-//         //complete this method
-//         if(head === null){
-//             head = 
-//         }  
-//     };
+	this.insert=function(head,data){
+        //complete this method
+        const newNode = new Node(data);
+        let currNode = head;
+        if(currNode !== null){
+            while(currNode.next !== null){
+                currNode = currNode.next;
+            }
+            currNode.next = newNode;
+        } else {
+            head = newNode;
+        }
+        return head;
+    };
 
-// 	this.display=function(head){
-//         var start=head;
-//             while(start){
-//                 process.stdout.write(start.data+" ");
-//                 start=start.next;
-//             }
-//     };
-// }
+	this.display=function(head){
+        var start=head;
+            while(start){
+                process.stdout.write(start.data+" ");
+                start=start.next;
+            }
+    };
+}
+
+const linkedList = Solution();
+linkedList.insert(4);
+linkedList.insert(2);
+linkedList.insert(3);
+linkedList.insert(4);
+linkedList.insert(1);
+linkedList.display();
