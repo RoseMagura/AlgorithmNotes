e791ed552c6cc485c553821f7d8d12b5d9acde76
RoseMagura <magurarm@guilford.edu>
2021-05-24 16:29:38 -0400

    Feat: Start working on bubble sort review

---
+++ javascript_review/bubbleSort.js
@@ -0,0 +1,10 @@
+const bubbleSort = arr => {
+    let numSwaps = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > arr[i + 1]{
+            console.log('inequality');
+        }
+    }
+}
+
+bubbleSort([3, 2, 1]);
