8169018012357b6d382a81d7524fdd2c8776ce74
RoseMagura <magurarm@guilford.edu>
2021-05-25 12:38:51 -0400

    Feat: Add my solution for self dividing problem

--- javascript_review/selfDividing.js
+++ javascript_review/selfDividing.js
@@ -1,11 +1,32 @@
-// Input: 
-// left = 1, right = 22
-// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+/** Input:
+* left = 1, right = 22
+* Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+*/
 
 const selfDividing = (left, right) => {
-    const output = [];
-    output.push(3);
+    const output = new Set();
+    let curr = left;
+    while (curr <= right) {
+        if (curr < 10) {
+            output.add(curr); // for numbers less than 10
+        } else {
+            let passing = true;
+            for(let i in String(curr)){
+                const digit = parseInt(String(curr)[i]);
+                if(digit === 0 || curr % digit !== 0){
+                    passing = false;
+                    break;
+                } 
+            }
+            if(passing){
+                output.add(curr);
+            }
+        }
+        curr++;
+    }
+    console.log(output);
     return output;
 }
 
 selfDividing(1, 22);
+selfDividing(10, 13);
