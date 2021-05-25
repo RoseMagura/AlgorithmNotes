2f35b9c83ec92ee088b3c5953e94f4f959ecd5dd
RoseMagura <magurarm@guilford.edu>
2021-05-25 12:40:59 -0400

    Feat: Add alternate solution for self dividing

--- javascript_review/selfDividing.js
+++ javascript_review/selfDividing.js
@@ -11,14 +11,14 @@ const selfDividing = (left, right) => {
             output.add(curr); // for numbers less than 10
         } else {
             let passing = true;
-            for(let i in String(curr)){
+            for (let i in String(curr)) {
                 const digit = parseInt(String(curr)[i]);
-                if(digit === 0 || curr % digit !== 0){
+                if (digit === 0 || curr % digit !== 0) {
                     passing = false;
                     break;
-                } 
+                }
             }
-            if(passing){
+            if (passing) {
                 output.add(curr);
             }
         }
@@ -26,6 +26,25 @@ const selfDividing = (left, right) => {
     }
     console.log(output);
     return output;
+}
+
+const alternateSolution = (left, right) => {
+    const result = [];
+    const selfDividingTest = num => {
+        return num.toString().split('').every(letter => {
+            if (Number(letter) === 0 || num % Number(letter) !== 0) {
+                return false;
+            }
+            return true;
+        })
+    }
+
+    for (let i = left; i <= right; i++) {
+        if (selfDividingTest(i)) {
+            result.push(i);
+        }
+    }
+    return result;
 }
 
 selfDividing(1, 22);
